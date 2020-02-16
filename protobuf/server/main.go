0cb9bf7f47407aa0c82658429c6d726ac4bbb645
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2020-02-16 13:11:23 +0900

    update

--- protobuf/server/main.go
+++ protobuf/server/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"github.com/tzmfreedom/go-sample/protobuf/helloworld"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
+	"net/http"
 )
 
 const (
@@ -21,10 +23,20 @@ func main() {
 	s := grpc.NewServer()
 	helloworld.RegisterGreeterServer(s, &server{})
 	reflection.Register(s)
-	err = s.Serve(lis)
-	if err != nil {
-		panic(err)
-	}
+	go s.Serve(lis)
+
+	wrappedGrpc := grpcweb.WrapServer(s, grpcweb.WithOriginFunc(func(origin string) bool { return true }))
+	http.ListenAndServe(":8080", http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		if wrappedGrpc.IsGrpcWebRequest(req) {
+			wrappedGrpc.ServeHTTP(resp, req)
+			return
+		} else if wrappedGrpc.IsAcceptableGrpcCorsRequest(req) {
+			wrappedGrpc.ServeHTTP(resp, req)
+			return
+		}
+		// Fall back to other servers.
+		http.DefaultServeMux.ServeHTTP(resp, req)
+	}))
 }
 
 type server struct {}
